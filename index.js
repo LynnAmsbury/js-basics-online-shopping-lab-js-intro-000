43d39f2a2716e76f5d2df984c980e5e203a116d4
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 23:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,9 +67,7 @@ function placeOrder(cardNumber) {
     //   }
 
     } else {
-      console.log('Sorry, we don\'t have a credit card on file for you.')
+      return 'Sorry, we don\'t have a credit card on file for you.'
       return cart;
     }
 }
-
-placeOrder();
