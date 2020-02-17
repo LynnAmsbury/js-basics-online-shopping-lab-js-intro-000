9dc8e01275e184241783e51e8593b3357ecef8be
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 22:24:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,12 +58,13 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    console.log('We don\'t have a credit card on file for you to place your order.');
+    console.log('Sorry, we don\'t have a credit card on file for you to place your order.');
   } else {
     console.log(`Your total cost is $${total}, which will be charged to the card ${cardNumber}.`);
     cart = [];
   }
 }
+placeOrder();
 
 // function placeOrder(cardNumber) {
 //   // write your code here
