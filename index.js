1cba75f54fb98454174ecbda2e77011ad52d2b5d
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 22:01:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,23 +56,23 @@ function removeFromCart(item) {
   }
 }
 
-function placeOrder(cardNumber) {
-  // write your code here
-  if (typeof(cardNumber) === 'null') {
-    return "Sorry, we don't have a credit card on file for you."
-} else {
-  cart = [];
-  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
-  }
-}
-
 // function placeOrder(cardNumber) {
-//
-//   if (typeof(cardNumber) === "undefined"){
-//     return "Sorry, we don't have a credit card on file for you."
-//   } else {
-//     cart = [];
-//     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
-//   }
 //   // write your code here
+//   if (typeof(cardNumber) === 'null') {
+//     return "Sorry, we don't have a credit card on file for you."
+// } else {
+//   cart = [];
+//   return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+//   }
 // }
+
+function placeOrder(cardNumber) {
+
+  if (typeof(cardNumber) === "undefined"){
+    return "Sorry, we don't have a credit card on file for you."
+  } else {
+    cart = [];
+    return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
+  }
+  // write your code here
+}
