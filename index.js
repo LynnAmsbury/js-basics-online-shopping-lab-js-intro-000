b2c9d8827cc97113092280eb2ba0c4383f48bdd5
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 22:40:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,9 +60,10 @@ function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--) {
-      cart.pop();
-    }
+    // for (var i = cart.length; i>= 0; i--) {
+    //   cart.pop();
+    //   }
+    cart = [];
     } else
       console.log('Sorry, we don\'t have a credit card on file for you.')
 return cart;
