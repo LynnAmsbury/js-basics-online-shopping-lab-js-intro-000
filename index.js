444415a0d6970d8b57a3d4a399d2153bdc4c5659
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 20:16:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function viewCart() {
   if (cart.length === 0) {
     return 'Your shopping cart is empty.'
   } else if (cart.length === 1) {
-    return 'In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.'
+    return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
   } else if (cart.length === 2) {
-    return 'In your cart, you have ${cart[0]} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.'
+    return `In your cart, you have ${cart[0]} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
   } else {
     var moreThanTwoItems = 'In your cart, you have '
     for (var i = 0; i < cart.length - 1; i++) {
