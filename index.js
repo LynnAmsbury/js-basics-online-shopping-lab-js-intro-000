b80a1b578840b6ccfacf773fd822c4f8b598cf3f
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 22:13:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber == undefined) {
+  if (!cardNumber) {
     console.log("Sorry, we don't have a credit card on file for you.")
-} else {
-  cart = [];
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+    return false;
   }
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = [];
 }
 
 // function placeOrder(cardNumber) {
