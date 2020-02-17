67217909d81dbc74d7841a51186c2c1f78fe0ad3
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 20:56:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ function viewCart() {
 function total() {
   // write your code here
   var total = 0;
-  for (t = 0; t < cart.length; t++) {
-    total += cart[t].itemPrice;
+  for (i = 0; i < cart.length; i++) {
+    total += cart[i].itemPrice;
   }
   return total;
 }
