840b764f382efccd23d4325f7dd5a1396a68d2d8
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 19:44:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  } else (cart.length === 1) {
+    return 'In your cart, you have ${itemObject.itemName[item]} at ${itemPrice[item].'
+  }
   for (var i = 0; i < cart.length; i++) {
     return `In your cart, you have ${itemObject.itemName[item]} at ${itemPrice[item]}.`
   }
