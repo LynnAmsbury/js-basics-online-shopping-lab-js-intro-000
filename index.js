5ddb60a704800d53ad4acb5e676b7e6718b4299d
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-17 22:16:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,13 +58,24 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (!cardNumber) {
-    console.log("Sorry, we don't have a credit card on file for you.")
-    return false;
+  if(cardNumber){
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    cart.splice(0, cart.length);
+  } else {
+    console.log("Sorry, we don't have a credit card on file for you.");
   }
-  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-  cart = [];
 }
+console.log(placeOrder());
+
+// function placeOrder(cardNumber) {
+//   // write your code here
+//   if (!cardNumber) {
+//     console.log("Sorry, we don't have a credit card on file for you.")
+//     return false;
+//   }
+//   console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+//   cart = [];
+// }
 
 // function placeOrder(cardNumber) {
 //
